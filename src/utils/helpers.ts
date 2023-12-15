281dfacf70b938afaa8662901aba03c9bc971410
chidioguejiofor <chidioguejiofor@gmail.com>
2023-12-15 17:47:55 +0100

    chore: improve keycloak env variable

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -56,6 +56,7 @@ export const sleep = (ms: number): Promise<void> => {
 };
 
 export const handleErrors = (context: Context, error: Error) => {
+  console.log(error, '<---errro');
   if (error instanceof EdcConnectorClientError) {
     context.set('Content-type', 'application/json');
     switch (error.type) {
